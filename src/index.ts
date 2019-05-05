f689e5e9083bcc82d8154664ab6ba1e7fe724421
Kirk Scheibelhut <kjs@scheibo.com>
2019-05-04 21:13:35 -0700

    Add Glicko

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 // import {Data, calcStat, toID} from 'ps';
 // const mon = Data.getSpecies(toID('genGAR'))!;
-// console.log(calcStat('spa', mon.baseStats.spa, 31, 252, 100, Data.Natures['modest']));
+// console.log(calcStat('spa', mon.baseStats.spa, 31, 252, 100,
+// Data.Natures['modest']));
