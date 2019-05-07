802581c69f2b829fc67f9f44d84df5b5a8343b68
Kirk Scheibelhut <kjs@google.com>
2019-05-07 10:35:08 -0700

    Trim index.d.ts to just whats needed

--- ps/index.d.ts
+++ ps/index.d.ts
@@ -1,19 +1,12 @@
 declare module 'ps' {
     type ID = '' | string & {__isID: true}
-    interface AnyObject {[k: string]: any}
-    type GenderName = 'M' | 'F' | 'N' | '';
-    type StatName = 'hp' | 'atk' | 'def' | 'spa' | 'spd' | 'spe';
-    type StatsTable = {[stat in StatName]: number };
-    type SparseStatsTable = Partial<StatsTable>;
-    type Nonstandard = 'Glitch' | 'Past' | 'Future' | 'CAP' | 'LGPE' | 'Pokestar' | 'Custom';
+    type StatName = 'hp' | 'atk' | 'def' | 'spa' | 'spd' | 'spe'
+    type StatsTable = {[stat in StatName]: number }
     interface Nature {
-        name: string;
-        plus?: keyof StatsTable;
-        minus?: keyof StatsTable;
-        [k: string]: any;
-    }
-    interface DexTable<T> {
-        [key: string]: T;
+        name: string
+        plus?: keyof StatsTable
+        minus?: keyof StatsTable
+        [k: string]: any
     }
     type PokemonSet = {
         name: string,
@@ -32,160 +25,36 @@ declare module 'ps' {
         hpType?: string,
     }
     interface Effect {
-        id: string
-        name: string
-        num: number
-        affectsFainted?: boolean
-        counterMax?: number
-        desc?: string
-        drain?: [number, number]
-        duration?: number
-        effectType?: string
-        infiltrates?: boolean
-        isNonstandard?: Nonstandard | null
-        isUnreleased?: boolean
-        isZ?: boolean | string
-        noCopy?: boolean
-        recoil?: [number, number]
-        shortDesc?: string
-        status?: string
-        weather?: string
-    }
-    type EffectType = 'Effect' | 'Pokemon' | 'Move' | 'Item' | 'Ability' | 'Format' | 'Ruleset' | 'Weather' | 'Status' | 'Rule' | 'ValidatorRule'
-    interface BasicEffect extends Effect {
         id: ID
-        weather?: ID
-        status?: ID
-        effectType: EffectType
+        name: string
         exists: boolean
-        flags: AnyObject
-        fullname: string
-        gen: number
-        sourceEffect: string
     }
-    interface Ability extends BasicEffect {
-        rating: number
-        isUnbreakable?: boolean
-        suppressWeather?: boolean
-    }
-    interface Item extends BasicEffect {
-        gen: number
-        forcedForme?: string
-        ignoreKlutz?: boolean
-        isBerry?: boolean
-        isChoice?: boolean
-        isGem?: boolean
+    interface Ability extends Effect {}
+    interface Item extends Effect {
         megaStone?: string
         megaEvolves?: string
-        naturalGift?: {basePower: number, type: string}
-        onDrive?: string
-        onMemory?: string
-        onPlate?: string
-        spritenum?: number
+
         zMove?: string | true
         zMoveFrom?: string
         zMoveType?: string
         zMoveUser?: string[]
     }
-    interface Move extends BasicEffect {
-        accuracy: true | number
-        basePower: number
-        category: 'Physical' | 'Special' | 'Status'
-        flags: AnyObject
-        pp: number
-        priority: number
-        target: string
-        type: string
-        alwaysHit?: boolean
-        baseMoveType?: string
-        basePowerModifier?: number
-        breaksProtect?: boolean
-        contestType?: string
-        critModifier?: number
-        critRatio?: number
-        damage?: number | 'level' | false | null
-        defensiveCategory?: 'Physical' | 'Special' | 'Status'
-        forceSwitch?: boolean
-        hasCustomRecoil?: boolean
-        heal?: number[] | null
-        ignoreAbility?: boolean
-        ignoreAccuracy?: boolean
-        ignoreDefensive?: boolean
-        ignoreEvasion?: boolean
-        ignoreImmunity?: boolean | {[k: string]: boolean}
-        ignoreNegativeOffensive?: boolean
-        ignoreOffensive?: boolean
-        ignorePositiveDefensive?: boolean
-        ignorePositiveEvasion?: boolean
-        isSelfHit?: boolean
-        isFutureMove?: boolean
-        isViable?: boolean
-        mindBlownRecoil?: boolean
-        multiaccuracy?: boolean
-        multihit?: number | number[]
-        multihitType?: string
-        noDamageVariance?: boolean
-        noFaint?: boolean
-        noMetronome?: string[]
-        nonGhostTarget?: string
-        noPPBoosts?: boolean
-        noSketch?: boolean
-        ohko?: boolean | string
-        pressureTarget?: string
-        pseudoWeather?: string
-        selfdestruct?: string | boolean
-        selfSwitch?: string | boolean
-        sideCondition?: string
-        sleepUsable?: boolean
-        slotCondition?: string
-        spreadModifier?: number
-        stallingMove?: boolean
-        stealsBoosts?: boolean
-        struggleRecoil?: boolean
-        terrain?: string
-        thawsTarget?: boolean
-        useTargetOffensive?: boolean
-        useSourceDefensive?: boolean
-        volatileStatus?: string
-        weather?: ID
-        willCrit?: boolean
-        forceSTAB?: boolean
-        zMovePower?: number
-        zMoveEffect?: string
-    }
-    type SpeciesAbility = {0: string, 1?: string, H?: string, S?: string}
-    interface Species extends BasicEffect {
-        abilities: SpeciesAbility
+    interface Move extends Effect {}
+    interface Species extends Effect {
+        abilities: {0: string, 1?: string, H?: string, S?: string}
         baseStats: StatsTable
-        canHatch?: boolean
-        color: string
-        eggGroups: string[]
-        heightm: number
-        num: number
         species: string
         types: string[]
-        weightkg: number
-        baseForme?: string
         baseSpecies?: string
-        evoLevel?: number
-        evoMove?: string
-        evoCondition?: string
-        evoItem?: string
-        evos?: string[]
-        evoType?: 'trade' | 'stone' | 'levelMove' | 'levelExtra' | 'levelFriendship' | 'levelHold'
         forme?: string
-        formeLetter?: string
-        gender?: GenderName
-        genderRatio?: {[k: string]: number}
-        maxHP?: number
-        otherForms?: string[]
-        otherFormes?: string[]
-        prevo?: string
     }
     interface Type {
         damageTaken: {[attackingTypeNameOrEffectid: string]: number}
-        HPdvs?: SparseStatsTable
-        HPivs?: SparseStatsTable
+        HPdvs?: Partial<StatsTable>
+        HPivs?: Partial<StatsTable>
+    }
+    interface DexTable<T> {
+        [key: string]: T
     }
     const Data: {
         Abilities: DexTable<Ability>
@@ -197,17 +66,17 @@ declare module 'ps' {
         Natures: DexTable<Nature>
         Types: DexTable<Type>
 
-        forFormat(format: string): typeof Data;
+        forFormat(format: string): typeof Data
 
-        getAbility(name: string): Ability | undefined;
-        getItem(name: string): Item | undefined;
-        getMove(name: string): Move | undefined;
-        getSpecies(name: string): Species | undefined;
+        getAbility(name: string): Ability | undefined
+        getItem(name: string): Item | undefined
+        getMove(name: string): Move | undefined
+        getSpecies(name: string): Species | undefined
         
-        getNature(name: string): Nature | undefined;
-        getType(name: string): Type | undefined;
+        getNature(name: string): Nature | undefined
+        getType(name: string): Type | undefined
     }
     function toID(text: any): ID
     function calcStat(stat: StatName, base: number, iv: number, ev: number, level: number, nature?: Nature): number
-    function unpackTeam(buf: string): PokemonSet[] | undefined;
+    function unpackTeam(buf: string): PokemonSet[] | undefined
 }
