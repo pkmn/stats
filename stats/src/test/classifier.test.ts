fc4eeb4fd7a948290f770bb55cb3051b28299828
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2024-08-28 21:38:50 -0400

    Satisfy linter

--- stats/src/test/classifier.test.ts
+++ stats/src/test/classifier.test.ts
@@ -1,7 +1,7 @@
+import {Generations, ID} from '@pkmn/data';
 import {Dex} from '@pkmn/dex';
 
 import * as classifier from '../classifier';
-import { Generations, ID } from '@pkmn/data';
 
 const GEN = new Generations(Dex).get(6);
 
@@ -16,36 +16,44 @@ describe('Classifier', () => {
     const COMPUTED_GREATER_MOVES = classifier.computeGreaterSetupMoves(GEN);
 
     // Moves that Antar incorrectly included (nothing)
-    expect(getDifference(classifier.GREATER_SETUP_MOVES, COMPUTED_GREATER_MOVES)).toEqual(new Set([]));
+    expect(getDifference(classifier.GREATER_SETUP_MOVES, COMPUTED_GREATER_MOVES))
+      .toEqual(new Set([]));
 
     // Moves that Antar forgot to include
-    expect(getDifference(COMPUTED_GREATER_MOVES, classifier.GREATER_SETUP_MOVES)).toEqual(new Set(['shellsmash', 'diamondstorm']));
+    expect(getDifference(COMPUTED_GREATER_MOVES, classifier.GREATER_SETUP_MOVES))
+      .toEqual(new Set(['shellsmash', 'diamondstorm']));
   });
   test('LESSER_SETUP_MOVES', () => {
     const COMPUTED_LESSER_SETUP_MOVES = classifier.computeLesserSetupMoves(GEN);
 
     // Moves that Antar incorrectly included (nothing)
-    expect(getDifference(classifier.LESSER_SETUP_MOVES, COMPUTED_LESSER_SETUP_MOVES)).toEqual(new Set([]));
+    expect(getDifference(classifier.LESSER_SETUP_MOVES, COMPUTED_LESSER_SETUP_MOVES))
+      .toEqual(new Set([]));
 
     // Moves that Antar forgot to include
-    expect(getDifference(COMPUTED_LESSER_SETUP_MOVES, classifier.LESSER_SETUP_MOVES)).toEqual(new Set(['honeclaws']));
+    expect(getDifference(COMPUTED_LESSER_SETUP_MOVES, classifier.LESSER_SETUP_MOVES))
+      .toEqual(new Set(['honeclaws']));
   });
   test('SETUP_MOVES', () => {
-    const COMPUTED_BATON_PASS_MOVES = classifier.computeBatonPassMoves(GEN);
+    const COMPUTED_SETUP_MOVES = classifier.computeBatonPassMoves(GEN);
 
     // Moves that Antar incorrectly included (nothing)
-    expect(getDifference(classifier.SETUP_MOVES, COMPUTED_BATON_PASS_MOVES)).toEqual(new Set([]));
+    expect(getDifference(classifier.SETUP_MOVES, COMPUTED_SETUP_MOVES))
+      .toEqual(new Set([]));
 
     // Moves that Antar forgot to include
-    expect(getDifference(COMPUTED_BATON_PASS_MOVES, classifier.SETUP_MOVES)).toEqual(new Set(['geomancy', 'diamondstorm', 'poweruppunch']));
+    expect(getDifference(COMPUTED_SETUP_MOVES, classifier.SETUP_MOVES))
+      .toEqual(new Set(['geomancy', 'diamondstorm', 'poweruppunch']));
   });
   test('GRAVITY_MOVES', () => {
     const COMPUTED_GRAVITY_MOVES = classifier.computeGravityMoves(GEN);
 
     // Moves that Antar incorrectly included despite having high accuracy
-    expect(getDifference(classifier.GRAVITY_MOVES, COMPUTED_GRAVITY_MOVES)).toEqual(new Set(['clamp', 'willowisp', 'psywave', 'rocktomb']));
+    expect(getDifference(classifier.GRAVITY_MOVES, COMPUTED_GRAVITY_MOVES))
+      .toEqual(new Set(['clamp', 'willowisp', 'psywave', 'rocktomb']));
 
     // Moves that Antar forgot to include
-    expect(getDifference(COMPUTED_GRAVITY_MOVES, classifier.GRAVITY_MOVES)).toEqual(new Set(['horndrill', 'precipiceblades', 'sandtomb', 'stickyweb', 'landswrath']))
+    expect(getDifference(COMPUTED_GRAVITY_MOVES, classifier.GRAVITY_MOVES))
+      .toEqual(new Set(['horndrill', 'precipiceblades', 'sandtomb', 'stickyweb', 'landswrath']));
   });
 });
